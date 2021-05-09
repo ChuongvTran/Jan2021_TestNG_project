cd9ef8e31e003c22ef2bd5f7e98a12e14e7ec7d3
ChuongvTran <chuongv.tran07@gmail.com>
2021-05-09 13:56:02 -0500

    fourth commit

--- src/main/java/vairousConcepts/GitTest1.java
+++ src/main/java/vairousConcepts/GitTest1.java
@@ -1,5 +1,31 @@
 package vairousConcepts;
 
 public class GitTest1 {
+	
+	public static void main(String[] args) {
+		
+		printMe();
+		
+	}
+	
+	public static void printMe() {
+		
+		System.err.println("git");
+	}
+	
+public static void printMe2() {
+		
+		System.err.println("git");
+	}
+
+public static void printMe3() {
+	
+	System.err.println("git");
+}
+
+public static void printMe4() {
+	
+	System.err.println("git");
+}
 
 }
