ddde83bfb607066e277c361d13c87717943dc806
ChuongvTran <chuongv.tran07@gmail.com>
2021-05-09 13:24:42 -0500

    second commit

---
+++ src/main/java/vairousConcepts/GitTest.java
@@ -0,0 +1,15 @@
+package vairousConcepts;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+	
+	public static void printMe() {
+		
+		System.out.println("git test");
+	}
+}
